320d7fb5c210a4979dc5b80c3c4f54c09dbf5687
Rob Blackbourn <rob.blackbourn@cityfinancial.co.uk>
2018-06-13 08:59:11 +0100

    Fixed up indentation

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 'babel-operator-overload-plugin disable'
 
 if (!Object.prototype.__add__) {
-    Object.prototype.__add__ = function (other) {
-      return this + other;
-    };
+  Object.prototype.__add__ = function (other) {
+    return this + other;
   }
+}
   
-  if (!Object.prototype.__sub__) {
-    Object.prototype.__sub__ = function (other) {
-      return this - other;
-    };
+if (!Object.prototype.__sub__) {
+  Object.prototype.__sub__ = function (other) {
+    return this - other;
   }
+}
   
-  if (!Object.prototype.__mul__) {
-    Object.prototype.__mul__ = function (other) {
-      return this * other;
-    };
+if (!Object.prototype.__mul__) {
+  Object.prototype.__mul__ = function (other) {
+    return this * other;
   }
+}
   
-  if (!Object.prototype.__div__) {
-    Object.prototype.__div__ = function (other) {
-      return this / other;
-    };
+if (!Object.prototype.__div__) {
+  Object.prototype.__div__ = function (other) {
+    return this / other;
   }
+}
   
-  if (!Object.prototype.__mod__) {
-    Object.prototype.__mod__ = function (other) {
-      return this % other;
-    };
+if (!Object.prototype.__mod__) {
+  Object.prototype.__mod__ = function (other) {
+    return this % other;
   }
+}
